84b6d0801f8eb48379b92a302dcf86aabad5f2fd
Carl Kittelberger <it@icedreammusic.eu>
2015-10-09 09:13:59 +0200

    Add parameter to start.sh to download authorized key off an HTTP server.

--- wheezy-backports-amd64/start.sh
+++ wheezy-backports-amd64/start.sh
@@ -3,5 +3,9 @@
 # this will regenerate server keys
 dpkg-reconfigure openssh-server
 
+# this will properly register the need authorized key
+curl "$1" >> ~jenkins/.ssh/authorized_keys
+chmod 600 ~jenkins/.ssh/*
+
 # this will start the SSH daemon in the foreground
 exec /usr/sbin/sshd -D -p 22
