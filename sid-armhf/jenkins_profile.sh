582864f71ef7965adcdd34aa420dacdfa46eba26
Carl Kittelberger <it@icedreammusic.eu>
2016-01-25 22:33:17 +0100

    Removing some excess packages.

---
+++ sid-armhf/jenkins_profile.sh
@@ -0,0 +1,2 @@
+echo "$(tput bold)$(tput setaf 7)>>> Updating package list...$(tput sgr0)"
+sudo apt-get update
---
+++ wheezy-backports-armhf/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+[ "$1" = "" ] && (echo "You need to define a URL to authorized keys." && exit 1)
+
+# this will regenerate server keys
+dpkg-reconfigure openssh-server
+
+# this will properly register the need authorized key
+echo "Downloading authorized key from $1..."
+curl --silent "$1" >> ~jenkins/.ssh/authorized_keys
+chown jenkins:jenkins -R ~jenkins/.ssh
+chmod 600 ~jenkins/.ssh/*
+
+# this will start the SSH daemon in the foreground
+exec /usr/sbin/sshd -D -p 22
